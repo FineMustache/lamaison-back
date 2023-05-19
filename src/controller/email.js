b6e0868ff2067dacd67b56866e9da8e80bc72184
FineMustache <guihenrique2806@gmail.com>
2023-05-19 10:00:45 -0300

    email correcto

--- src/controller/email.js
+++ src/controller/email.js
@@ -17,9 +17,25 @@ function send() {
     }
   });
 
+  console.log({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.MAIL_USERNAME,
+
+      pass: process.env.MAIL_PASSWORD,
+
+      clientId: process.env.OAUTH_CLIENTID,
+
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN
+    }
+  })
+
   let mailOptions = {
     from: 'lamaison.suporte@gmail.com',
-    to: 'mitsueyanoo@gmail.com',
+    to: 'guihenrique2806@gmail.com',
     subject: 'Nodemailer Project',
     text: 'Hi from your nodemailer project',
     html: '<h1>Teste</h1>'
